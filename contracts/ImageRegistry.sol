9d3096662e68f9cc7df54f22e5fdbfb304d3b2ac
Trevor Clarke <besnoid@gmail.com>
2018-10-04 02:01:01 -0700

    Removed complexities

--- contracts/ImageRegistry.sol
+++ contracts/ImageRegistry.sol
@@ -11,38 +11,19 @@ contract ImageRegistry {
   event AssetAdded(address indexed ref, uint64 id, address sender);
   event AssetRemoved(address indexed ref, address sender);
 
-  struct Asset {
-    address ref;
-    uint64 id;
-  }
-
-  mapping(address => Asset) public allAssets;
+  mapping(address => uint) public allAssets;
 
   constructor() public {
     owner = msg.sender;
   }
 
-  modifier onlyAdmin {
-    require(owner == msg.sender);
-    _;
+  function addAsset(address ref, uint64 id) public {
+    allAssets[ref] = id;
+    emit AssetAdded(ref, id, msg.sender);
   }
 
-  modifier assetExists(address ref) {
-    require(allAssets[ref].ref != 0);
-    _;
-  }
-
-  function addAsset(address _ref, uint64 _id) public {
-    allAssets[_ref] = Asset(_ref, _id);
-    emit AssetAdded(_ref, _id, msg.sender);
-  }
-
-  function removeAsset(address ref) public onlyAdmin assetExists(ref) {
+  function removeAsset(address ref) public {
     delete allAssets[ref];
     emit AssetRemoved(ref, msg.sender);
-  }
-
-  function getAssetByHash(address ref) public view returns (address, uint64) {
-    return (allAssets[ref].ref, allAssets[ref].id);
   }
 }
