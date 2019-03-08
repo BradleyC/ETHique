42705106caf69feabd3b77b758115f87c13362d9
Bradley C <bradleyctclarke@gmail.com>
2019-03-08 23:34:36 +0100

    removing demo dapp stuff

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,21 @@
 <template>
   <div class="container">
-    <h2>ImageRegistry</h2>
-
-    <div class="box">
-      <h3>Add Asset</h3>
-      <div class="form">
-        <input type="text" name="ref" v-model="newRef" placeholder="Image Address">
-        <input type="text" name="id" v-model="newId" placeholder="Image ID">
-        <button class="btn" @click="addAssetItem">Submit</button>
-      </div>
-    </div>
-
-    <div class="list">
-      <div class="list-item" v-for="item in items">
-        <div class="asset">
-          <img :src="getImagePath(item)">
-        </div>
-        <div class="copy">
-          <h4>Ref: {{ item.ref }}</h4>
-          <p>ID: {{ item.id }}</p>
-        </div>
-        <button class="btn" @click="deleteAsset(item)">Delete</button>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Home',
 
   data() {
-    return {
-      newRef: '0x06012c8cf97bead5deae237070f9587f8e7a266d',
-      newId: 1066650
-    }
-  },
+    },
 
   computed: {
-    ...mapGetters(['items'])
   },
 
   methods: {
-    ...mapActions(['removeAsset', 'addAsset']),
-    getImagePath(item) {
-      // return `https://img.cryptokitties.co/${item.ref}/${item.id}.svg`
-      return `https://storage.googleapis.com/opensea-prod.appspot.com/${
-        item.ref
-      }/${item.id}.svg`
-    },
-    deleteAsset(item) {
-      this.removeAsset(item)
-    },
-    addAssetItem() {
-      if (!this.newRef || !this.newId) return
-      this.addAsset({ ref: this.newRef, id: parseInt(this.newId) })
-    }
+    
   }
 }
 </script>
