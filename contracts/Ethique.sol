fe4e6d958a84fed96fc5cdaa65dfcafa5fca9fe4
Bradley C <bradleyctclarke@gmail.com>
2019-03-09 11:57:47 +0100

    added ranking algo

--- contracts/Ethique.sol
+++ contracts/Ethique.sol
@@ -9,9 +9,9 @@ contract Ethique {
     // storage 
     string public name;
     string public symbol;
-    uint8 public decimals = 0;
     uint256 public totalSupply;
     address[] public registeredUsers;
+    uint8 public decimals = 0;
 
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
