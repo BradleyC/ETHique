3eeb773c6e4e3f450fc6f1fde2294045b3aa9cc0
gabrielibarra <gabej.ibarra@gmail.com>
2019-03-09 12:13:27 +0100

    gi/serverside login api sets and returns address to UI

--- config/webpack/dev.env.js
+++ config/webpack/dev.env.js
@@ -3,4 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  SERVER: '"https://od8h51vl3b.execute-api.eu-west-2.amazonaws.com/test/v1"'
+  // TWITTER: '"W3ZNv1aXsgdy3IvG4z9RWYHmg"'
 })
